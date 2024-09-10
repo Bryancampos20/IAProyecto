36c3778b64c8dcb1171657794e7314e759d3df63
Bryan Campos <62086944+Bryancampos20@users.noreply.github.com>
2024-09-09 18:48:02 -0600

    Vizualizacion del balance de clases

--- proyecto_tecnicas_clasificacion_IA.py
+++ proyecto_tecnicas_clasificacion_IA.py
@@ -1,24 +1,41 @@
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # Cargar los datos de diabetes
 diabetes_data = pd.read_csv('diabetes.csv')
 
 # Cargar el segundo conjunto de datos
-surgical_deepnet_data = pd.read_csv('Surgical-deepnet.csv')
+surgical_data = pd.read_csv('Surgical-deepnet.csv')
 
 """
 # Ver las primeras filas de cada dataset
 print(diabetes_data.head())
-print(surgical_deepnet_data.head())
-"""
+print(surgical_data.head())
 
-"""
 # Resumen estadístico
 print(diabetes_data.describe())
-print(surgical_deepnet_data.describe())
+print(surgical_data.describe())
 
-"""
 
 # Comprobar si hay valores faltantes
 print(diabetes_data.isnull().sum())
-print(surgical_deepnet_data.isnull().sum())
+print(surgical_data.isnull().sum())
+
+
+# Revisar las estadísticas descriptivas
+print(diabetes_data.describe())
+print(surgical_data.describe())
+
+"""
+
+# Visualización de la distribución de clases para el dataset de Diabetes
+sns.countplot(x='Outcome', data=diabetes_data)
+plt.title('Distribución de Clases en el Dataset de Diabetes')
+plt.show()
+
+# Visualización de la distribución de clases para el dataset quirúrgico
+sns.countplot(x='Outcome', data=surgical_data)
+plt.title('Distribución de Clases en el Dataset Quirúrgico')
+plt.show()
