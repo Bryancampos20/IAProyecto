00323e3cbd6eed59f1a6753ba1b5a10967b5aeee
Bryan Campos <62086944+Bryancampos20@users.noreply.github.com>
2024-09-09 18:38:05 -0600

    Using a complete dataset

--- proyecto_tecnicas_clasificacion_IA.py
+++ proyecto_tecnicas_clasificacion_IA.py
@@ -4,5 +4,21 @@ import pandas as pd
 diabetes_data = pd.read_csv('diabetes.csv')
 
 # Cargar el segundo conjunto de datos
-heart_data = pd.read_csv('heart_disease_uci.csv')
+surgical_deepnet_data = pd.read_csv('Surgical-deepnet.csv')
 
+"""
+# Ver las primeras filas de cada dataset
+print(diabetes_data.head())
+print(surgical_deepnet_data.head())
+"""
+
+"""
+# Resumen estadístico
+print(diabetes_data.describe())
+print(surgical_deepnet_data.describe())
+
+"""
+
+# Comprobar si hay valores faltantes
+print(diabetes_data.isnull().sum())
+print(surgical_deepnet_data.isnull().sum())
