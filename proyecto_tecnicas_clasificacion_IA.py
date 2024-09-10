e0f64151d86de8b4ed491c2785aff0128325a3ed
Bryan Campos <62086944+Bryancampos20@users.noreply.github.com>
2024-09-09 18:20:45 -0600

    Cargar datos csv

---
+++ proyecto_tecnicas_clasificacion_IA.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Cargar los datos de diabetes
+diabetes_data = pd.read_csv('diabetes.csv')
+
+# Cargar el segundo conjunto de datos
+heart_data = pd.read_csv('heart_disease_uci.csv')
+
